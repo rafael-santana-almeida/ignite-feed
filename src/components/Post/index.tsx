b570bcce033a5254f0aba13f41c7376b90b990df
Rafael Santana <rafaelsantana.devweb@gmail.com>
2024-05-05 15:13:09 -0300

    Adiciona estrutura e estilização do formulário de comentários do componente Post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -30,6 +30,16 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu comentário</strong>
+
+        <textarea placeholder='Deixe seu comentário' />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
